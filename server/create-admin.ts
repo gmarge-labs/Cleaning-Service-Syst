c7018c5e53baa2b84a6b924284805bd0cd4d8abd
Ayshatouh <aishakabir964@gmail.com>
2026-01-11 18:11:42 +0100

    Fix create-admin.ts import path

--- server/create-admin.ts
+++ server/create-admin.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Role } from '@prisma/client';
 import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
